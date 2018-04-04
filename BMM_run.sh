b0928466b54868adc2a553af25d67498f0911356
carstencww <carstencww@gmail.com>
2018-04-04 17:01:03 +0800

    finish run script

--- BMM_run.sh
+++ BMM_run.sh
@@ -5,7 +5,7 @@ IN_FILE=/user/ChanCarsten/input_BMM/image_train.txt
 BMM () {
 hadoop dfs -rm -R $OUT_FILE
 hadoop jar /usr/hdp/current/hadoop-mapreduce-client/hadoop-streaming.jar \
--D mapred.map.tasks=10 \
+-D mapred.map.tasks=20 \
 -D mapred.reduce.tasks=10 \
 -file $PWD/train_paras.txt \
 -file $PWD/map_BMM.py -mapper map_BMM.py \
@@ -18,7 +18,7 @@ echo $PWD
 BMM
 converging=( $(./check_converge.py) )
 while [ ${converging[0]} = 1 ]; do
-mv paras_result.txt train_centroid.txt
+mv paras_result.txt train_paras.txt
 echo ${converging[1]} >> log.txt
 BMM
 converging=( $(./check_converge.py) )
